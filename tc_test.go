51b5a8a93ec96a51d19807db9f14de4857d32808
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-03-26 00:25:28 +0000

    added multiple echo test

--- tc_test.go
+++ tc_test.go
@@ -119,6 +119,87 @@ func TestSendReceiveWithEOL(t *testing.T) {
 	close(closed)
 }
 
+func TestSendReceiveMultipleWithEOL(t *testing.T) {
+	ln, err := nettest.NewLocalListener("tcp")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ln.Close()
+	_, port, err := net.SplitHostPort(ln.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	config := &Config{
+		MaxFrameBytes: 512,
+		Destination:   net.JoinHostPort("", port),
+	}
+
+	closed := make(chan struct{})
+
+	c := New(config)
+
+	go c.Run(closed)
+
+	time.Sleep(time.Millisecond)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer func() {
+		ln.Close()
+		wg.Wait()
+	}()
+
+	// Echo first line of a message.
+	go func() {
+		c, err := ln.Accept()
+		if err != nil {
+			return
+		}
+		for i := 0; i < 3; i++ {
+
+			rb := bufio.NewReader(c)
+			line, err := rb.ReadString('\n')
+			if err != nil {
+				t.Error(err)
+				c.Close()
+				continue
+			}
+			log.Infof("localListener received message %s", line)
+			if _, err := c.Write([]byte(line)); err != nil {
+				t.Error(err)
+			}
+			log.Infof("localListener sent message %s", line)
+		}
+		c.Close()
+
+		wg.Done()
+	}()
+
+	time.Sleep(time.Millisecond)
+
+	greeting := []byte("Greetings\n")
+	for i := 0; i < 3; i++ {
+		select {
+		case <-time.After(time.Millisecond):
+			t.Errorf("Timeout on Send")
+		case c.Send <- greeting:
+		}
+		select {
+		case <-time.After(10 * time.Millisecond):
+			t.Errorf("Timeout on Receive")
+		case msg, ok := <-c.Receive:
+			if !ok {
+				t.Errorf("Channel problem")
+			}
+			if bytes.Compare(msg, greeting) != 0 {
+				t.Errorf("Wrong message. Want: %s\nGot : %s\n", greeting, msg)
+			}
+		}
+	}
+	close(closed)
+}
+
 /*
 func TestReceiveStream(t *testing.T) {
 
